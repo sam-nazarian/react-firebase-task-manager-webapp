31d9c44847a4796e3039ef010a81693a467527ec
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-01 13:36:00 -0600

    Used the Custom Http Hook In App.js

--- src/App.js
+++ src/App.js
@@ -2,39 +2,22 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-// import useHttp from './components/hooks/use-http';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  // useHttp();
-
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://react-http-4f104-default-rtdb.firebaseio.com/tasks.json');
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+  // function will be called in useHttp hook, the place that calls the func sets parameters of func
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({ url: 'https://react-http-4f104-default-rtdb.firebaseio.com/tasks.json' }, transformTasks);
 
   useEffect(() => {
     fetchTasks();
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -9,9 +9,9 @@ const useHttp = (requestConfig, applyData) => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
@@ -20,13 +20,8 @@ const useHttp = (requestConfig, applyData) => {
 
       const data = await response.json();
 
-      // change the transformation logic
+      // change the transformation logic. When trying to use the hook, it will have the parameter of data on it
       applyData(data);
-      // const loadedTasks = [];
-      // for (const taskKey in data) {
-      //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      // }
-      // setTasks(loadedTasks);
     } catch (err) {
       setError(err.message || 'Something went wrong!');
     }
